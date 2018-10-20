99015aef283cd42d852cbd3a0ccbb3b2e2dad377
Adam <aaadam@mail.com>
2018-10-20 09:59:24 +0800

    继续学习中

--- base/001_NameSpace.py
+++ base/001_NameSpace.py
@@ -28,3 +28,18 @@
 # 123
 # abc
 # -------------------------------------------
+
+
+# 2.当函数内有一变量名和某个外部函数同名，在该函数内部调用外部同名函数可使用globals,
+	# globals() 和 locals() 提供了基于字典的访问全局和局部变量的方式
+# def a():
+# 	return 'it is a'
+# 	# print('it is a')
+# def fun():
+# 	a = 'it is fun'
+# 	f1 = globals()['a']()
+# 	print(a)
+# 	print(f1)
+# 	print(globals())
+# 	print(locals())
+# fun()
